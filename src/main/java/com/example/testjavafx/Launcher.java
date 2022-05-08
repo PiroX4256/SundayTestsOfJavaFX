355bb809058c45fc4f049f8c2a81b6bdd4de539d
Luca Pirovano <luca.pirovano.98@gmail.com>
2022-05-08 17:14:47 +0200

    commit

---
+++ src/main/java/com/example/testjavafx/Launcher.java
@@ -0,0 +1,10 @@
+package com.example.testjavafx;
+
+import com.example.testjavafx.HelloApplication;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        HelloApplication.main(null);
+    }
+}
